a20063081b9e0e9d31a8716d2e4c217c0bd2399c
wilkdasilk <anwilk92@gmail.com>
2017-01-29 13:15:09 -0800

    prototype home page

---
+++ myScript.js
@@ -0,0 +1,6 @@
+//reference jQuery
+var script = document.createElement('script');
+script.src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js";
+script.type = "text/javascript";
+document.getElementsByTagName('head')[0].appendChild(script);
+
